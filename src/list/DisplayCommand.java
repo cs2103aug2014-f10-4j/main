2738757f3676ea9eaf93cd249aff5c421a2f9af7
michael <mike.bdg@gmail.com>
2014-10-07 23:42:32 +0800

    Add EditCommand, DeleteCommand, DisplayCommand minimally for parser to use

---
+++ src/list/DisplayCommand.java
@@ -0,0 +1,16 @@
+package list;
+
+public class DisplayCommand implements ICommand {
+	private Integer mTaskNumber;
+
+	public DisplayCommand(Integer taskNumber) {
+		mTaskNumber = taskNumber;
+	}
+	
+	@Override
+	public String execute() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
