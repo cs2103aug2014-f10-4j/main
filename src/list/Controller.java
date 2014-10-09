723de59a5f5df7b8e43df6e2ce84b74740768887
michael <mike.bdg@gmail.com>
2014-10-09 15:06:00 +0800

    Add Controller class, without any method implementations

---
+++ src/list/Controller.java
@@ -0,0 +1,17 @@
+package list;
+
+public class Controller {
+		
+	private IUserInterface mUserInterface = null;
+	
+	public static void main(String[] args) {
+					
+		while (true) {
+			
+		}
+	}
+
+	public static void displayTasksInUI() {
+		
+	}
+}
