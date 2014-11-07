60d57f7da28fbdbb511909ae8b369c255c00370d
michael <mike.bdg@gmail.com>
2014-11-08 03:06:03 +0800

    Add Constants.java in util package

---
+++ src/list/util/Constants.java
@@ -0,0 +1,7 @@
+package list.util;
+
+public class Constants {
+	public static final String FLOATING_TASKS = "FLOATING TASKS";
+	public static final String CURRENT_TASKS = "CURRENT TASKS";
+	public static final String OVERDUE_TASKS = "OVERDUE TASKS";
+}
